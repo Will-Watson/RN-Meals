a435577a161a554d3d10d8923c5fde396b1ebb4d
William Watson <williampwatson1991@gmail.com>
2024-01-11 19:56:59 -0500

    feat: CategoriesScreen dummy component created. CATEGORIES dummy data imported

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const CategoriesScreen = () => {
+  return (
+    <View>
+      <Text>CategoriesScreen</Text>
+    </View>
+  );
+};
+
+export default CategoriesScreen;
