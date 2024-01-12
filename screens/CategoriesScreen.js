f12d3c752d1cf98fcf25dbdd19c502fa5387ce59
William Watson <williampwatson1991@gmail.com>
2024-01-11 20:25:09 -0500

    feat: styling added to newly created components. statusbar changed to light mode for visibility

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -7,11 +7,16 @@ const renderCategoryItem = (itemData) => {
   return (
     <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
   );
-}
+};
 
 const CategoriesScreen = () => {
   return (
-    <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
   );
 };
 
