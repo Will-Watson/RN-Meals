50a0d627b5a24340ad93914ed4959cb0d3de39c7
William Watson <williampwatson1991@gmail.com>
2024-01-14 18:17:33 -0500

    feat: finishing touches on some styling with the meal details screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
@@ -9,7 +9,7 @@ const MealDetailsScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
-    <View>
+    <ScrollView style={styles.rootContainer}>
       <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
       <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
@@ -18,15 +18,22 @@ const MealDetailsScreen = ({ route, navigation }) => {
         affordability={selectedMeal.affordability}
         textStyle={styles.detailText}
       />
-      <SubTitle>Ingredients</SubTitle>
-      <List data={selectedMeal.ingredients} />
-      <SubTitle>Steps</SubTitle>
-      <List data={selectedMeal.steps} />
-    </View>
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <SubTitle>Ingredients</SubTitle>
+          <List data={selectedMeal.ingredients} />
+          <SubTitle>Steps</SubTitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
   image: {
     width: '100%',
     height: 350,
@@ -40,6 +47,12 @@ const styles = StyleSheet.create({
   },
   detailText: {
     color: 'white',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    maxWidth: '80%',
   },
 });
 
