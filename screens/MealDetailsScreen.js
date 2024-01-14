079c76a8728b4a58e63886f3e6db93dc6fd42933
William Watson <williampwatson1991@gmail.com>
2024-01-14 17:29:38 -0500

    feat: meal title and image added to details screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,11 +1,14 @@
 import { View, Text, StyleSheet, Image } from 'react-native';
 
+import { MEALS } from '../data/dummy-data';
+
 const MealDetailsScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View>
-      <Image />
-      <Text>Meal Details Screen {mealId}</Text>
+      <Image source={{uri: selectedMeal.imageUrl}}/>
+      <Text>{selectedMeal.title}</Text>
       {/* <View style={styles.details}>
         <Text style={styles.detailItem}>{duration}m</Text>
         <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
