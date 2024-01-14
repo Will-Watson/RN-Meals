85ee2b4a35ac1892078ce5067584349a54ed024b
William Watson <williampwatson1991@gmail.com>
2024-01-14 17:27:30 -0500

    feat: place holders added to MealDetailsScreen component

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,10 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const MealDetailsScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
   return (
     <View>
+      <Image />
       <Text>Meal Details Screen {mealId}</Text>
+      {/* <View style={styles.details}>
+        <Text style={styles.detailItem}>{duration}m</Text>
+        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+      </View> */}
+      <Text>Ingredients</Text>
+      <Text>Steps</Text>
     </View>
   );
 };
