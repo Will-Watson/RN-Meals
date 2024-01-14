b2289822c3dc4652634c0306f11aa65066b02253
William Watson <williampwatson1991@gmail.com>
2024-01-14 17:54:58 -0500

    feat: border added to subtitles with a container as well as color change

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -14,14 +14,19 @@ const MealDetailsScreen = ({ route, navigation }) => {
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
+      <View style={styles.subTitleContainer}>
+        <Text style={styles.subTitle}>Ingredients</Text>
+      </View>
       <Text>
         {selectedMeal.ingredients.map((ingredient) => (
           <Text key={ingredient}>{ingredient}</Text>
         ))}
       </Text>
-      <Text>Steps</Text>
+      <View style={styles.subTitleContainer}>
+        <Text style={styles.subTitle}>Steps</Text>
+      </View>
       <Text>
         {selectedMeal.steps.map((step) => (
           <Text key={step}>{step}</Text>
@@ -42,7 +47,23 @@ const styles = StyleSheet.create({
     margin: 8,
     textAlign: 'center',
     color: 'white',
-  }
+  },
+  detailText: {
+    color: 'white',
+  },
+  subTitleContainer: {
+    padding: 6,
+    borderBottomColor: '#e2b497',
+    borderBottomWidth: 2,
+    marginHorizontal: 24,
+    marginVertical: 4,
+  },
+  subTitle: {
+    color: '#e2b497',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
 export default MealDetailsScreen;
