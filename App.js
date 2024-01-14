10d3076d0518841eedc09a7a59a5e2d7e6b6a91b
William Watson <williampwatson1991@gmail.com>
2024-01-14 18:20:59 -0500

    feat: dummy favorite button added to header in meal details screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -49,6 +49,11 @@ export default function App() {
           <Stack.Screen
             name='MealDetails'
             component={MealDetailsScreen}
+            options={{
+              headerRight: () => {
+                return <Button title='Fav' onPress={() => {}} />;
+              },
+            }}
           />
         </Stack.Navigator>
       </NavigationContainer>
