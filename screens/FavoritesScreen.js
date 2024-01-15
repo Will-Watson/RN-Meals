ffb0c618a88d997fbb90c5fe2de8f34f5a20c2ad
William Watson <williampwatson1991@gmail.com>
2024-01-15 13:57:51 -0500

    feat: message added to favorites screen to let the user know that they have no favorites if that is the case

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 import { FavoritesContext } from '../store/context/favorites-context';
@@ -10,7 +11,31 @@ const FavoritesScreen = () => {
   const favoriteMeals = MEALS.filter((meal) =>
     favoriteMealsContext.ids.includes(meal.id)
   );
+
+  if (favoriteMeals.length === 0 || !favoriteMeals) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>
+          No favorite meals found. Start adding some!
+        </Text>
+      </View>
+    );
+  }
+
   return <MealsList items={favoriteMeals} />;
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
 export default FavoritesScreen;
